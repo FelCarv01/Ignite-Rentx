63287d7d9560e14a6633ce124565be8bb03e48ba
Felipe Carvalho <feliped3carvalho@gmail.com>
2021-09-06 20:58:41 -0300

    introducao ao singleton pattern

--- src/modules/cars/useCases/listCategory/index.ts
+++ src/modules/cars/useCases/listCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepositoy";
 import { ListCategoriesController } from "./listCategoriesController";
 import { ListCategoriesUseCase } from "./listCategoriesUseCase";
 
-const categoriesReposity = new CategoriesRepository();
+const categoriesReposity = CategoriesRepository.getInstance();
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesReposity);
 const listCategoriesController = new ListCategoriesController(
   listCategoriesUseCase
